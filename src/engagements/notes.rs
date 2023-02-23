f1dcba3d0595aba2aba8af43d173c23f84177756
Toni Sefton <50565228+tonisefton@users.noreply.github.com>
2023-02-23 20:58:20 +1000

    remove Notes

--- src/engagements/notes.rs
+++ src/engagements/notes.rs
@@ -1,16 +1,6 @@
 use serde::Serialize;
 use time::OffsetDateTime;
 
-/// Notes add information to the record timeline or associate an attachment with an object.
-/// For example, if you need to keep track of an offline conversation you had with a contact,
-/// you can add a note to their contact record with details and documents related to the conversation.
-/// Other users in the account will then be able to view and reference that note.
-#[derive(Serialize, Debug)]
-pub struct Note {
-    pub associations: Vec<Association>,
-    pub properties: NoteProperties,
-}
-
 #[derive(Serialize, Debug)]
 pub struct NoteProperties {
     /// The note's text content, limited to 65,536 characters.
